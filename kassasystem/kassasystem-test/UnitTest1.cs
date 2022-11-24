e98fa533a1445d495bc4083b7e0753ae5787ce0f
Linus J <linus.jansson2000@gmail.com>
2022-11-24 10:35:23 +0100

    added button click tests
    Co-Authored-By: Gustavsson123 <111958017+Gustavsson123@users.noreply.github.com>
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: lukas-laas <111971517+lukas-laas@users.noreply.github.com>

--- kassasystem/kassasystem-test/UnitTest1.cs
+++ kassasystem/kassasystem-test/UnitTest1.cs
@@ -4,20 +4,36 @@ namespace kassasystem_test
 {
     public class Tests
     {
-        
+        kassasystem.Form1 _form;
+
         [SetUp]
         public void Setup()
         {
+            _form = new kassasystem.Form1();
         }
 
         [Test]
         public void test_clickpersonbutton()
         {
-            kassasystem.Form1 _form = new kassasystem.Form1();
-
             _form.btn_person.PerformClick();
 
-            Assert.That(_form.listBox1.Items.Contains(_form.btn_person.Text), Is.True);
+            Assert.That(!_form.listBox1.Items.Contains(_form.btn_person.Text), Is.True);
+        }
+
+        [Test]
+        public void test_clickroombutton()
+        {
+            _form.btn_room.PerformClick();
+
+            Assert.That(!_form.listBox1.Items.Contains(_form.btn_room.Text), Is.True);
+        }
+
+        [Test]
+        public void test_clickclearbutton()
+        {
+            _form.btn_clear.PerformClick();
+
+            Assert.That((_form.listBox1.Items.Count == 0), Is.True);
         }
     }
 }
