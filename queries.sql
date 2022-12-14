534d1416ad5a777987319ceed3ecc9c47c9df168
Linus J <linus.jansson2000@gmail.com>
2022-12-14 14:22:08 +0100

    logic to fix overlapping checkin & checkout dates

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 
 -- get all rooms that are not booked for a given date range (startDate, endDate)
-SELECT *
+SELECT r.floor, r.roomNumber, r.rate, b.dateFrom, b.dateTo, r2.type
 FROM rooms r 
 	LEFT JOIN roomsBooked r1 ON ( r1.roomID = r.roomID  )  
 	LEFT JOIN bookings b ON ( b.bookingID = r1.bookingID  )  
 	LEFT JOIN roomTypes r2 ON ( r2.roomTypesID = r.roomTypesID  )  
-WHERE (r.roomID NOT IN (SELECT roomID FROM roomsBooked)) OR NOT (b.dateFrom <= @startDate AND @endDate <= b.dateTo);
+WHERE (r.roomID NOT IN (SELECT roomID FROM roomsBooked)) OR NOT (@checkIn <= b.dateTo AND @checkoutDate >= b.dateFrom);
