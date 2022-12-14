199ac4d69eb723b6d17e98a9a929728c883817f1
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2022-12-14 14:26:47 +0100

    Interface for new bookings

--- kassasystem/kassasystem/UserControlBooking.Designer.cs
+++ kassasystem/kassasystem/UserControlBooking.Designer.cs
@@ -28,19 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.bookings = new System.Windows.Forms.ListBox();
+            this.availableRooms = new System.Windows.Forms.ListBox();
+            this.btnNewBooking = new System.Windows.Forms.Button();
+            this.btnEditBooking = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.inputFirstName = new System.Windows.Forms.TextBox();
+            this.inputLastName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Location = new System.Drawing.Point(1389, 14);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(279, 27);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker2.Location = new System.Drawing.Point(1111, 14);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(272, 27);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // bookings
+            // 
+            this.bookings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.bookings.FormattingEnabled = true;
+            this.bookings.ItemHeight = 15;
+            this.bookings.Location = new System.Drawing.Point(13, 14);
+            this.bookings.Name = "bookings";
+            this.bookings.Size = new System.Drawing.Size(406, 1009);
+            this.bookings.TabIndex = 2;
+            // 
+            // availableRooms
+            // 
+            this.availableRooms.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.availableRooms.FormattingEnabled = true;
+            this.availableRooms.ItemHeight = 15;
+            this.availableRooms.Location = new System.Drawing.Point(583, 14);
+            this.availableRooms.Name = "availableRooms";
+            this.availableRooms.Size = new System.Drawing.Size(522, 1009);
+            this.availableRooms.TabIndex = 3;
+            this.availableRooms.Visible = false;
+            // 
+            // btnNewBooking
+            // 
+            this.btnNewBooking.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNewBooking.Location = new System.Drawing.Point(425, 92);
+            this.btnNewBooking.Name = "btnNewBooking";
+            this.btnNewBooking.Size = new System.Drawing.Size(152, 72);
+            this.btnNewBooking.TabIndex = 4;
+            this.btnNewBooking.Text = "New booking";
+            this.btnNewBooking.UseVisualStyleBackColor = true;
+            this.btnNewBooking.Click += new System.EventHandler(this.btnNewBooking_Click);
+            // 
+            // btnEditBooking
+            // 
+            this.btnEditBooking.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEditBooking.Location = new System.Drawing.Point(425, 14);
+            this.btnEditBooking.Name = "btnEditBooking";
+            this.btnEditBooking.Size = new System.Drawing.Size(152, 72);
+            this.btnEditBooking.TabIndex = 5;
+            this.btnEditBooking.Text = "Edit booking";
+            this.btnEditBooking.UseVisualStyleBackColor = true;
+            this.btnEditBooking.Visible = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(1111, 101);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(152, 72);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // inputFirstName
+            // 
+            this.inputFirstName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputFirstName.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputFirstName.Location = new System.Drawing.Point(1111, 63);
+            this.inputFirstName.Name = "inputFirstName";
+            this.inputFirstName.PlaceholderText = "First name";
+            this.inputFirstName.Size = new System.Drawing.Size(272, 27);
+            this.inputFirstName.TabIndex = 7;
+            this.inputFirstName.UseWaitCursor = true;
+            this.inputFirstName.Visible = false;
+            // 
+            // inputLastName
+            // 
+            this.inputLastName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputLastName.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputLastName.Location = new System.Drawing.Point(1389, 63);
+            this.inputLastName.Name = "inputLastName";
+            this.inputLastName.PlaceholderText = "Last name";
+            this.inputLastName.Size = new System.Drawing.Size(279, 27);
+            this.inputLastName.TabIndex = 8;
+            this.inputLastName.Visible = false;
             // 
             // UserControlBooking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
+            this.Controls.Add(this.inputLastName);
+            this.Controls.Add(this.inputFirstName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEditBooking);
+            this.Controls.Add(this.btnNewBooking);
+            this.Controls.Add(this.availableRooms);
+            this.Controls.Add(this.bookings);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Name = "UserControlBooking";
             this.Size = new System.Drawing.Size(1671, 1030);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private ListBox bookings;
+        private ListBox availableRooms;
+        private Button btnNewBooking;
+        private Button btnEditBooking;
+        private Button btnSave;
+        private TextBox inputFirstName;
+        private TextBox inputLastName;
     }
 }
--- kassasystem/kassasystem/UserControlBooking.cs
+++ kassasystem/kassasystem/UserControlBooking.cs
@@ -16,5 +16,24 @@ namespace kassasystem
         {
             InitializeComponent();
         }
+        private void btnNewBooking_Click(object sender, EventArgs e)
+        {
+            btnSave.Show();
+            inputFirstName.Show();
+            inputLastName.Show();
+            availableRooms.Show();
+            dateTimePicker1.Show();
+            dateTimePicker2.Show();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            btnSave.Hide();
+            inputFirstName.Hide();
+            inputLastName.Hide();
+            availableRooms.Hide();
+            dateTimePicker1.Hide();
+            dateTimePicker2.Hide();
+        }
     }
 }
